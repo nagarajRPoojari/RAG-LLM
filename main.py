b3eebd210e498159f1c030518d9c99b152475735
nagarajRPoojari <np137270@gmail.com>
2023-11-15 12:32:25 +0530

    model added

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import random
 import time
 
 
-import RAG_LLM.pipeline
+from RagLLM.pipeline import RagBot
 
 with gr.Blocks() as demo:
     chatbot = gr.Chatbot(height=1000)
     msg = gr.Textbox()
     clear = gr.ClearButton([msg, chatbot])
-
+    bot=RagBot()
+    
     def respond(message, chat_history):
         bot_message = random.choice(["How are you?", "I love you", "I'm very hungry"])
         chat_history.append((message, bot_message))
